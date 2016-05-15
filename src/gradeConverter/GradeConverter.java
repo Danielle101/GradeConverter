8f4dc4bfe983a8a503751f0a956b3a40b047093f
DH <halldan4@yahoo.com>
2016-05-15 11:55:51 -0500

    first commit

---
+++ src/gradeConverter/GradeConverter.java
@@ -0,0 +1,11 @@
+package gradeConverter;
+
+public class GradeConverter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the Letter Grade Converter!");
+System.out.println("Enter a numerical grade (0-100):");
+}
+
+}
