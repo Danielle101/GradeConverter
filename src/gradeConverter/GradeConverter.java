afb4fb1e3cb2c6071337dd0e6a00ed3b06d87082
DH <halldan4@yahoo.com>
2016-05-15 12:45:53 -0500

    added last half of grade ranges, added continuation message

--- src/gradeConverter/GradeConverter.java
+++ src/gradeConverter/GradeConverter.java
@@ -18,12 +18,25 @@ int grade = scan.nextInt();
 if (grade > 92){ System.out.println("Letter grade: A");}
 else if (grade >89 && grade < 93){System.out.println("Letter grade: A-");}
 else if (grade >86 && grade < 90){System.out.println("Letter grade: B+");}
+else if (grade >82 && grade < 87){System.out.println("Letter grade: B");}
+else if (grade >79 && grade < 83){System.out.println("Letter grade: B-");}
+else if (grade >76 && grade < 80){System.out.println("Letter grade: C+");}
+else if (grade >72 && grade < 77){System.out.println("Letter grade: C");}
+else if (grade >69 && grade < 73){System.out.println("Letter grade: C-");}
+else if (grade >66 && grade < 70){System.out.println("Letter grade: D+");}
+else if (grade >62 && grade < 67){System.out.println("Letter grade: D");}
+else if (grade >59 && grade < 63){System.out.println("Letter grade: D-");}
+else if ( grade < 60){System.out.println("Letter grade: F");}
+
+System.out.println("Would you like to enter another grade?(y/n):");
+choice = scan.next();
+
 
 }
 
 
 
-
+System.out.println("Thank you for using the Grade Converter App! Buh-bye!");
 
 
 
